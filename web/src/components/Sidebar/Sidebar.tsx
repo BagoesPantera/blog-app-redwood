513e237329fe4e2d8c7b3546e9d9e9fdf0f01ffc
pantera <bagoespantera01@gmail.com>
2024-07-04 07:23:56 +0800

    add logout

--- web/src/components/Sidebar/Sidebar.tsx
+++ web/src/components/Sidebar/Sidebar.tsx
@@ -2,10 +2,13 @@ import { useEffect, useState } from 'react'
 
 import { IconButton } from '@mui/material'
 
-import { navigate, routes, useLocation } from "@redwoodjs/router";
+import { navigate, routes, useLocation } from '@redwoodjs/router'
+
+import { useAuth } from 'src/auth'
 
 const Sidebar = () => {
   const location = useLocation()
+  const { logOut } = useAuth()
   const [selectedUrl, setSelectedUrl] = useState('')
 
   useEffect(() => {
@@ -24,16 +27,13 @@ const Sidebar = () => {
       label: 'My Blog',
       url: '/my-blog',
       icon: <></>,
-    }
+    },
   ]
   return (
     <div className="stiky left-0 top-24 mr-4 h-screen w-72 rounded-r-lg bg-red-100">
       <div className="flex flex-col items-start space-y-1">
         {menu.map((item) => (
-          <div
-            key={item.id}
-            className={`w-full rounded-r-lg pl-4 `}
-          >
+          <div key={item.id} className={`w-full rounded-r-lg pl-4 `}>
             <IconButton
               className=" m-auto flex"
               sx={{
@@ -43,12 +43,25 @@ const Sidebar = () => {
               }}
               onClick={() => navigate(item.url)}
             >
-              <p className={` ml-4 mt-1 text-[16px]`}>
-                {item.label}
-              </p>
+              <p className={` ml-4 mt-1 text-[16px]`}>{item.label}</p>
             </IconButton>
           </div>
         ))}
+        <div className={`w-full rounded-r-lg pl-4 `}>
+          <IconButton
+            className=" m-auto flex"
+            sx={{
+              '&:hover': {
+                backgroundColor: 'transparent',
+              },
+            }}
+            onClick={async () => {
+              await logOut()
+            }}
+          >
+            <p className={` ml-4 mt-1 text-[16px]`}>Logout</p>
+          </IconButton>
+        </div>
       </div>
     </div>
   )
